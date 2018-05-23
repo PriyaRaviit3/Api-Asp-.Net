f6d867c3d4151cf6c1c4c93828f8a06d307a4d0a
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-05-23 12:00:36 +0530

    Add files via upload

---
+++ ItemsDbContext.cs
@@ -0,0 +1,24 @@
+using Demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Data
+{
+    public class ItemsDbContext : DbContext
+    {
+        public ItemsDbContext(DbContextOptions<ItemsDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Items> items { get; set; }
+       /* protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Convention.Remove<PluralizingTableNameConvention>();
+        }*/
+    
+    }
+}
